704950927d7f9a154e0b37b7b9833ee413d73213
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-08-14 23:32:47 +0100

    Add CSharpier and GH Actions (Linter and Size Label) (#4)

--- Src/POCAsterisk/Controllers/WeatherForecastController.cs
+++ Src/POCAsterisk/Controllers/WeatherForecastController.cs
@@ -8,8 +8,17 @@ namespace POCAsterisk.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing",
+            "Bracing",
+            "Chilly",
+            "Cool",
+            "Mild",
+            "Warm",
+            "Balmy",
+            "Hot",
+            "Sweltering",
+            "Scorching"
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -21,13 +30,18 @@ namespace POCAsterisk.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return Enumerable
+                .Range(1, 5)
+                .Select(
+                    index =>
+                        new WeatherForecast
+                        {
+                            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                            TemperatureC = Random.Shared.Next(-20, 55),
+                            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                        }
+                )
+                .ToArray();
         }
     }
 }
