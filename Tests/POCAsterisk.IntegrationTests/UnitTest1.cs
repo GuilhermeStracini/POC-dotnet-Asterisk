478cbf1b67786a10dce911e0c472095f506708fa
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-08-15 01:12:15 +0100

    Add tests (#6)
    * Add tests
    
    * Add tests

---
+++ Tests/POCAsterisk.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace POCAsterisk.IntegrationTests;
+
+[ExcludeFromCodeCoverage]
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
